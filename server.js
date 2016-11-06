1955c2443c559708c2ce243e20f8de8d6da667e8
Damola <d.labi@yahoo.com>
2016-11-06 18:25:58 -0500

    relearning handlebars

--- server.js
+++ server.js
@@ -1,8 +1,18 @@
 var express = require ('express');
 var app = express();
+var exphbs = require ('express3-handlebars');
+app.engine('handlebars', 
+	exphbs({defaultLayout: 'main'}));
+app.set('view engine', 'handlebars');
 
 app.get('/', function(req, res) {
-	res.send('Hello World!');
+	res.render('index', {
+		name: 'World!'
+	});
+});
+
+app.get('/about', function(req, res) {
+	res.render('about');
 });
 
 app.use('/public', express.static('public'));
